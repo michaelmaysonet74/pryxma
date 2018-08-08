9ac4163e4921dd049d60e8077ceeaf1d3384cfa0
Michael Maysonet <michaelmaysonet74@gmail.com>
2018-08-08 17:31:15 -0400

    Changed footer copyright year

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
 	return (
 		<div className="footer container container-fluid">
 			<p className="text-center"> 
-				© 2017 pryxma.com. All Rights Reserved.
+				© 2018 pryxma.com. All Rights Reserved.
 			</p>
 		</div>
 	);
